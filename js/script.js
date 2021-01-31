2903f000df0582ce9325025206bd1e69e6a3b03f
Jason Nutt <jasonnutt@Jasons-MacBook-Pro.local>
2021-01-31 12:09:53 -0500

    js commit

--- js/script.js
+++ js/script.js
@@ -6,11 +6,11 @@ var c = canvas.getContext('2d');
 c.fillStyle = 'rgba(255, 0, 0, 0.5)';
 c.fillRect(100, 400, 100, 100);
 c.fillStyle = 'skyblue';
-c.fillRect(300, 100, 100, 100);
+c.fillRect(400, 100, 100, 100);
 c.fillStyle = 'rgba(444, 0, 111, 0.5)';
 c.fillRect(500, 200, 100, 100);
 c.fillStyle = 'rgba(333, 0, 222, 0.5)';
-c.fillRect(600, 500, 100, 100);
+c.fillRect(600, 700, 100, 100);
 console.log(canvas)
 
 // line
@@ -19,7 +19,7 @@ c.beginPath();
 c.moveTo(150, 300);
 c.lineTo(300, 100);
 c.lineTo(400, 300);
-c.lineTo(300, 200);
+c.lineTo(600, 200);
 c.lineTo(300, 600);
 c.strokeStyle = "green";
 c.stroke();
