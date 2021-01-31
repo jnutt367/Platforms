8ff773a62b7e6104b9f9b7dbdfd52b6663dc6e55
Jason Nutt <jasonnutt@Jasons-MacBook-Pro.local>
2021-01-30 22:49:24 -0500

    4 squares and up arrow HTML5

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
 var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+c.fillStyle = 'rgba(255, 0, 0, 0.5)';
+c.fillRect(100, 400, 100, 100);
+c.fillStyle = 'skyblue';
+c.fillRect(300, 100, 100, 100);
+c.fillStyle = 'rgba(444, 0, 111, 0.5)';
+c.fillRect(500, 200, 100, 100);
+c.fillStyle = 'rgba(333, 0, 222, 0.5)';
+c.fillRect(600, 500, 100, 100);
 console.log(canvas)
+
+// line
+
+c.beginPath();
+c.moveTo(150, 300);
+c.lineTo(300, 100);
+c.lineTo(400, 300);
+c.lineTo(300, 200);
+c.lineTo(300, 600);
+c.strokeStyle = "green";
+c.stroke();
