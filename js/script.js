c64a4a413127c323392db4cfad630674e3d30115
Jason Nutt <jnutt367@gmail.com>
2021-02-01 13:16:08 -0500

    commit js green animated circle top fold

--- js/script.js
+++ js/script.js
@@ -3,20 +3,20 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
-c.fillStyle = 'rgba(255, 0, 0, 0.5)';
-c.fillRect(100, 400, 200, 200);
-c.fillStyle = 'skyblue';
-c.fillRect(400, 100, 100, 100);
-c.fillStyle = 'rgba(444, 0, 111, 0.5)';
-c.fillRect(500, 300, 150, 150);
-c.fillStyle = 'rgba(333, 0, 222, 0.5)';
-c.fillRect(600, 700, 100, 100);
-c.fillStyle = 'rgba(535, 333, 444, 0.5)';
-c.fillRect(400, 800, 100, 100);
-c.fillStyle = 'rgba(333, 0, 222, 0.5)';
-c.fillRect(100, 100, 50, 50);
-c.fillStyle = 'rgba(333, 111, 222, 0.5)';
-c.fillRect(250, 550, 100, 100);
+// c.fillStyle = 'rgba(255, 0, 0, 0.5)';
+// c.fillRect(100, 400, 200, 200);
+// c.fillStyle = 'skyblue';
+// c.fillRect(400, 100, 100, 100);
+// c.fillStyle = 'rgba(444, 0, 111, 0.5)';
+// c.fillRect(500, 300, 150, 150);
+// c.fillStyle = 'rgba(333, 0, 222, 0.5)';
+// c.fillRect(600, 700, 100, 100);
+// c.fillStyle = 'rgba(535, 333, 444, 0.5)';
+// c.fillRect(400, 800, 100, 100);
+// c.fillStyle = 'rgba(333, 0, 222, 0.5)';
+// c.fillRect(100, 100, 50, 50);
+// c.fillStyle = 'rgba(333, 111, 222, 0.5)';
+// c.fillRect(250, 550, 100, 100);
 
 console.log(canvas)
 
@@ -37,11 +37,33 @@ c.stroke();  */
 //c.strokeStyle = "#fa34a3";
 //c.stroke();
 
-for (var i = 0; i < 300; i++) {
-    var x = Math.random() * window.innerWidth;
-    var y = Math.random() * window.innerHeight;
+//for (var i = 0; i < 300; i++) {
+//    var x = Math.random() * window.innerWidth;
+//    var y = Math.random() * window.innerHeight;
+//    c.beginPath();
+//    c.arc(x, y, 30, 0, Math.PI * 2, false);
+//    c.strokeStyle = "blue";
+//    c.stroke();
+//}
+
+//c.beginPath();
+//c.arc(300, 300, 100, 0, Math.PI * 2, false);
+//c.strokeStyle = "#fa34a3";
+//c.stroke();
+
+
+var x = 200;
+
+function animate() {
+    requestAnimationFrame(animate);
+
     c.beginPath();
-    c.arc(x, y, 30, 0, Math.PI * 2, false);
-    c.strokeStyle = "blue";
+    c.arc(x, 200, 200, 30, 0, Math.PI * 2, false);
+    c.strokeStyle = 'green';
     c.stroke();
+
+
+    x += 1;
+
 }
+animate();
