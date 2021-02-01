97b0acc1b549f9bc960f08a4754c53cbd2e2f8ef
Jason Nutt <jnutt367@gmail.com>
2021-02-01 16:06:08 -0500

    ball now bounces off of right wall at radius center...

--- js/script.js
+++ js/script.js
@@ -3,33 +3,31 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
-// c.fillStyle = 'rgba(255, 0, 0, 0.5)';
-// c.fillRect(100, 400, 200, 200);
-// c.fillStyle = 'skyblue';
-// c.fillRect(400, 100, 100, 100);
-// c.fillStyle = 'rgba(444, 0, 111, 0.5)';
-// c.fillRect(500, 300, 150, 150);
-// c.fillStyle = 'rgba(333, 0, 222, 0.5)';
-// c.fillRect(600, 700, 100, 100);
-// c.fillStyle = 'rgba(535, 333, 444, 0.5)';
-// c.fillRect(400, 800, 100, 100);
-// c.fillStyle = 'rgba(333, 0, 222, 0.5)';
-// c.fillRect(100, 100, 50, 50);
-// c.fillStyle = 'rgba(333, 111, 222, 0.5)';
-// c.fillRect(250, 550, 100, 100);
-
-console.log(canvas)
+//c.fillStyle = 'rgba(255, 0, 0, 0.5)';
+//c.fillRect(100, 400, 200, 200);
+//c.fillStyle = 'skyblue';
+//c.fillRect(400, 100, 100, 100);
+//c.fillStyle = 'rgba(444, 0, 111, 0.5)';
+//c.fillRect(500, 300, 150, 150);
+//c.fillStyle = 'rgba(333, 0, 222, 0.5)';
+//c.fillRect(600, 700, 100, 100);
+//c.fillStyle = 'rgba(535, 333, 444, 0.5)';
+//c.fillRect(400, 800, 100, 100);
+//c.fillStyle = 'rgba(333, 0, 222, 0.5)';
+//c.fillRect(100, 100, 50, 50);
+//c.fillStyle = 'rgba(333, 111, 222, 0.5)';
+//c.fillRect(250, 550, 100, 100);
+//console.log(canvas)
 
 // line
-/*
-c.beginPath();
-c.moveTo(0, 0);
-c.lineTo(500, 500);
-c.lineTo(1000, 0);
-c.lineTo(1100, 200);
-c.lineTo(0, 100);
-c.strokeStyle = "green";
-c.stroke();  */
+//c.beginPath();
+//c.moveTo(0, 0);
+//c.lineTo(500, 500);
+//c.lineTo(1000, 0);
+//c.lineTo(1100, 200);
+//c.lineTo(0, 100);
+//c.strokeStyle = "green";
+//c.stroke();
 
 // Arc / Circle
 //c.beginPath();
@@ -38,21 +36,15 @@ c.stroke();  */
 //c.stroke();
 
 //for (var i = 0; i < 300; i++) {
-//    var x = Math.random() * window.innerWidth;
-//    var y = Math.random() * window.innerHeight;
-//    c.beginPath();
-//    c.arc(x, y, 30, 0, Math.PI * 2, false);
-//    c.strokeStyle = "blue";
-//    c.stroke();
-//}
-
-//c.beginPath();
-//c.arc(300, 300, 100, 0, Math.PI * 2, false);
-//c.strokeStyle = "#fa34a3";
-//c.stroke();
-
+//  var x = Math.random() * window.innerWidth;
+//  var y = Math.random() * window.innerHeight;
+//  c.beginPath();
+//  c.arc(x, y, 30, 0, Math.PI * 2, false);//  c.strokeStyle = "blue";
+//  c.stroke();
+// }
 
 var x = 200;
+var dx = 5;
 
 function animate() {
     requestAnimationFrame(animate);
@@ -63,8 +55,10 @@ function animate() {
     c.strokeStyle = 'green';
     c.stroke();
 
-
-    x += 1;
+    if (x > innerWidth) {
+        dx = -dx;
+    }
+    x += dx;
 
 }
 animate();
