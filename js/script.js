519f9bcfeee0ff92e9f9767b06a9708619b77f15
Jason Nutt <jnutt367@gmail.com>
2021-02-02 20:37:03 -0500

    changed to purple and accounted for circle radius

--- js/script.js
+++ js/script.js
@@ -54,7 +54,7 @@ function Circle(x, y, dx, dy, radius) {
     this.draw = function() {
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        c.strokeStyle = 'gold';
+        c.strokeStyle = 'purple';
         c.stroke();
     }
 
@@ -77,7 +77,7 @@ function Circle(x, y, dx, dy, radius) {
 var circleArray = [];
 
 for (var i = 0; i < 100; i++) {
-    var x = Math.random() * innerWidth;
+    var x = Math.random() * (innerWidth - radius * 2) + radius;
     var y = Math.random() * innerHeight;
     var dx = (Math.random() - 0.5) * 5;
     var dy = (Math.random() - 0.5) * 5;
