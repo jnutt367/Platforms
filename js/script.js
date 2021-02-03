fe147b2c40d4847faf4ed7b8cd2f534c24394ac5
Jason Nutt <jnutt367@gmail.com>
2021-02-02 21:30:53 -0500

    Gold and violet for the Lords victory

--- js/script.js
+++ js/script.js
@@ -54,9 +54,9 @@ function Circle(x, y, dx, dy, radius) {
     this.draw = function() {
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        c.strokeStyle = 'red';
+        c.strokeStyle = 'purple';
         c.stroke();
-        c.fillStyle = 'black'
+        c.fillStyle = 'gold'
         c.fill();
     }
 
@@ -81,8 +81,8 @@ var circleArray = [];
 for (var i = 0; i < 100; i++) {
     var x = Math.random() * (innerWidth - radius * 2) + radius;
     var y = Math.random() * (innerHeight - radius * 2) + radius;
-    var dx = (Math.random() - 0.5) * 5;
-    var dy = (Math.random() - 0.5) * 5;
+    var dx = (Math.random() - 0.5) * 35;
+    var dy = (Math.random() - 0.5) * 45;
     var radius = 100;
     circleArray.push(new Circle(x, y, dx, dy, radius));
 }
